c8593cec496a01d6c37fe3b136032ac1a716e786
Jose Daniel Lara <jdlara@berkeley.edu>
2025-06-03 17:38:44 -0600

    add includes

--- src/CDMParser.jl
+++ src/CDMParser.jl
@@ -1,9 +1,16 @@
 module CDMParser
+import InfrastructureSystems
+
+const IS = InfrastructureSystems
+
 using DocStringExtensions
 
 @template (FUNCTIONS, METHODS) = """
                                  $(TYPEDSIGNATURES)
                                  $(DOCSTRING)
                                  """
+
+include("enums.jl")
+include("power_system_table_data.jl")
 
 end
